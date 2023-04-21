332496ffbc5b840ef723cf8d12eb92922eeb5560
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-21 17:36:32 +0800

    Add random stuff

--- BSCPE 1-5 MALABO RENIEL A. - OOP ASSIGNMENT - DECRYPTION.py
+++ BSCPE 1-5 MALABO RENIEL A. - OOP ASSIGNMENT - DECRYPTION.py
@@ -2,6 +2,7 @@
 #Subject - Object Oriented Programming      #Problem 2 - DECRYPTION
 
 #Write a Python Script that will accept a string as encrypted text and then the program will decrypt it using the following character substitute:'a' = *, 'e' = & , 'i' = # , 'o' = + 'u' = !
+print("\033[1;35m Th#s pr+gr*m #s m*de t+ d&crypt m&ss*g&s th*t s!bst#t!t&s th& ch*r*ct&rs '*' as 'a', '&' as 'e', '#' as 'i', '+' as 'o', and '!' as 'u'.")
 print("\033[1;35m This program is made to decrypt messages that substitutes the characters '*' as 'a', '&' as 'e', '#' as 'i', '+' as 'o', and '!' as 'u'.")
 #ask user what they want to decrypt
 code_encrypted=input('What do you want to deciper? Type the message here: ')
